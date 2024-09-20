0bc6b2269dff86f13c78d97794164c864db56b72
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-09-20 14:07:16 +0300

    resolved conflicts

--- Totosteps/child/models.py
+++ Totosteps/child/models.py
@@ -1,18 +1,3 @@
-# from django.db import models
-# from django.utils.dateformat import DateFormat
-
-# class Child(models.Model):
-#     child_id = models.AutoField(primary_key=True)
-#     username = models.CharField(max_length=20, unique=True)
-#     date_of_birth = models.DateField()
-#     profile_picture = models.ImageField(upload_to='child_profiles/', blank=True, null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)  
-#     updated_at = models.DateTimeField(auto_now=True)
-
-
-#     def __str__(self):
-#         return f"{self.username} (ID: {self.child_id})"
-
 from django.db import models
 
 class Child(models.Model):
