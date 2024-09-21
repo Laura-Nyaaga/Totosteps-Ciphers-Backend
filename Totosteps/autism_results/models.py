9ea30e74b612a3d9863338b98c3a3cce084647c9
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-09-21 18:14:29 +0300

    add migrations

--- Totosteps/autism_results/models.py
+++ Totosteps/autism_results/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 from autism_image.models import Autism_Image
-from django.utils.timezone import now
 
 class Autism_Results(models.Model):
     results_id = models.IntegerField()
     image_id = models.ForeignKey(Autism_Image, on_delete=models.CASCADE)
     result = models.CharField(max_length=80)
-    updated_at = models.DateTimeField(default=now)
-    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField()
 
     def __str__(self):
         return f"{self.result}"
