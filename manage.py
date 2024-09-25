3dda66e2285dcd7ddf5bb12dbee6fc5dad86313b
Joyce Ndichu <ndichujoyce8@gmail.com>
2024-09-25 22:00:28 +0300

    Revert "updated manage.py with new changes"

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'totosteps.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Totosteps.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
